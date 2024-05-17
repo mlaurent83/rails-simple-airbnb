b4e92f7d637d6b8fc2e28437dacd6a5944d54a70
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-05-17 14:31:46 +0100

    implemented simple airbnb functions

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
+gem "simple_form"
 
 ruby "3.1.2"
 
---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,49 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      # Redirect to the index action
+      redirect_to flats_path, notice: 'Flat was successfully created.'
+    else
+      # Render the 'new' template again with error messages
+      render :new
+    end
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to @flat, notice: 'Flat was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
+  end
+end
